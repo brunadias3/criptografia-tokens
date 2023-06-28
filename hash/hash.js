23407047a912ab98d6c4cb90614dcd72ac538479
Bruna <brunadias_30@hotmail.com>
2023-06-28 16:38:24 -0300

    estudo hash

--- hash/hash.js
+++ hash/hash.js
@@ -1,1 +1,31 @@
-import {createHash} from 'crypto'
+import { createHash } from 'crypto'
+
+function criaHash(senha) {
+    return createHash('sha256').update(senha).digest('hex')
+}
+
+console.log(criaHash("uma String Qualquer"))
+
+class Usuario {
+    constructor(nome, senha) {
+        this.nome = nome;
+        this.hash = criaHash(senha);
+    }
+
+    autentica(nome, senha) {
+        if (nome === this.nome && this.hash === criaHash(senha)) {
+            console.log("Usuário autenticado com sucesso!");
+            return true;
+        }
+
+        console.log("Usuário ou senha incorretos.");
+        return false;
+    }
+}
+
+const usuario = new Usuario('bruna', 'minhaSenha')
+
+console.log(usuario)
+
+usuario.autentica('bruna', 'minhaSenha')
+usuario.autentica('bruna', 'minhasenha')
