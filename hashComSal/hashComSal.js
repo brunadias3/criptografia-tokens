448aaa5624fb813c6c516574b18f122db25db9cc
Bruna <brunadias_30@hotmail.com>
2023-06-29 11:58:48 -0300

    explicacao

--- hashComSal/hashComSal.js
+++ hashComSal/hashComSal.js
@@ -34,3 +34,9 @@ console.log(br)
 br.autentica('bruna','minhasenha') //teste de nome errado
 br.autentica('Bruna', 'minhasenha') //teste de senha errada
 br.autentica('Bruna', 'minhaSenha') //teste de sucesso
+
+//Por ser aleatório e modificar toda e qualquer senha, 
+//o “sal” na função de hash faz com que senhas iguais tenham hashes diferentes, 
+//fazendo com que atacantes não consigam realizar o ataque de rainbow table 
+//por não identificar senhas a partir de hashes gerados, 
+//pois toda senha terá um hash diferente. Isso dificulta muito o ataque por fazer com que os atacantes tenham que adivinhar não mais apenas a senha, como também o “sal” utilizado para gerar a hash da senha, acarretando em infinitas combinações extremamente difíceis de serem acertadas.
